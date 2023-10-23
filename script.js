0293eb5dab6f6cf4e814c7f7365f5e21754f9a31
RangelR2 <grangellemes@gmail.com>
2023-10-22 22:32:23 -0300

    Att

--- script.js
+++ script.js
@@ -12,14 +12,23 @@ pageTitle.setAttribute('style','margin: 1%;  margin-left: 15%; margin-right: 15%
 const pageCanvas = document.createElement('div');
 pageCanvas.classList.add('pageCanvas');
 content.appendChild(pageCanvas);
-pageCanvas.setAttribute('style','margin: 1%; margin-left: 15%; margin-right: 15%; height: 83vh; border: 2px solid yellow; display: flex;');
+pageCanvas.setAttribute('style','display: flex; margin: 1%; margin-left: 15%; margin-right: 15%; height: 83vh; border: 2px solid yellow; display: flex;');
 
-const buttonsDiv = document.createElement('div');
-buttonsDiv.classList.add('buttonsDiv');
+const buttonsDiv = document.createElement('buttonsDiv');
+buttonsDiv.classList.add('div');
 pageCanvas.appendChild(buttonsDiv);
-buttonsDiv.setAttribute('style','padding: 3%; padding-right: 25%; margin-left: 5%; margin: 1%; width: 4%: height: 83vh; border: 2px solid purple;');
+buttonsDiv.setAttribute('style', 'margin: 1%; padding-right: 15%; font-family: "Roboto", sans-serif; border: 2px solid black; display: flex; height: 97%; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
+
+// ('style', 'font-family: "Roboto", sans-serif; border: 2px solid black; display: flex;
+// height: 100%; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
 
 const CanvasDiv = document.createElement('div');
 CanvasDiv.classList.add('CanvasDiv');
 pageCanvas.appendChild(CanvasDiv);
-CanvasDiv.setAttribute('style','padding-right: 67%; margin: 1%; width: 550px: height: 83vh; border: 2px solid blue;');
+CanvasDiv.setAttribute('style','padding-right: 71%; margin: 1%; height: 97%; border: 2px solid blue;');
+
+const resetButton = document.createElement('resetButton');
+resetButton.classList.add('button');
+resetButton.textContent = 'Start!';
+buttonsDiv.appendChild(resetButton);
+resetButton.setAttribute('style', 'font-family: "Roboto", sans-serif; background-color: white; padding:20px; padding-left: 30px; padding-right: 30px; border-radius: 15px;font-size: 20px; cursor: pointer; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
