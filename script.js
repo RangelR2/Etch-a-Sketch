85ea1133b8d3dd9d1f4183dfcac84c18e2fc95e1
RangelR2 <grangellemes@gmail.com>
2023-10-25 22:44:05 -0300

    Att

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-function fillBoard(size){
-let board = document.querySelector(".canvasDiv");
-let squares = board.querySelector('div')
-squares.forEach((div => div.remove()));
-board.style.gridTemplateColumns = `repeat(${size} 1fr)`;
-board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+function fillBoard(size) {
+    let board = document.querySelector(".canvasDiv");
+    let squares = board.querySelectorAll('div')
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-let amount = size * size;
-for(let i = 0; i < amount; i++){
-    let square = document.createElement('div');
-    square.setAttribute('style','background-color: white;');
-    board.insertAdjacentElement("beforeend", square);
-    console.log("???222")
-}
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', () => {
+            square.setAttribute('style', 'background-color: rgb(255, 0, 127);');
+        });
+        square.setAttribute('style', 'background-color: pink;');
+        board.insertAdjacentElement("beforeend", square);
+
+        const blackButton = document.querySelector('#blackButton');
+        blackButton.addEventListener('click', () => {
+            console.log('teste');
+            square.setAttribute('style', 'background-color: black;');
+        });
+
+    }
 }
 
 fillBoard(16);
 
-function changeSize(input){
-    if(input <= 100 || input > 1){
+function changeSize(input) {
+    if (input <= 100 || input > 1) {
         fillBoard(input);
-        console.log("???")
-    } else{
-        alert("mt quadrado")
+    } else {
+        alert("mt quadrado");
     }
 }
+
+const sizeButton = document.querySelector('#setSize');
+sizeButton.addEventListener('mouseover', () => {
+    sizeButton.setAttribute('style', 'background-color: rgb(180,180,180); cursor: pointer;');
+});
+
+sizeButton.addEventListener('mouseout', () => {
+    sizeButton.setAttribute('style', 'background-color: white;');
+});
+
