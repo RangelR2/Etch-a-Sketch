f6fe0a8cf0b33b25145fc38d39435e135b83920c
RangelR2 <grangellemes@gmail.com>
2023-10-26 16:34:13 -0300

    Att

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function fillBoard(size) {
     for (let i = 0; i < amount; i++) {
         let square = document.createElement('div');
         square.addEventListener('mouseover', () => {
-            square.setAttribute('style', 'background-color: blue;');
+            square.setAttribute('style', 'background-color: black;');
         });
         square.setAttribute('style', 'background-color: white;');
         board.insertAdjacentElement("beforeend", square);
@@ -34,19 +34,22 @@ function fillBoard(size) {
                 square.setAttribute('style', 'background-color: white;');
             });
         });
+        resetButton.addEventListener('click', () => {
+            square.setAttribute('style', 'background: white;');
+            console.log('teste');
+        });
+        const randomButton = document.querySelector('#randomButton');
+        randomButton.addEventListener('click', () => {
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+            });
+        });
     }
 }
 
-fillBoard(16);
 
-function changeSize(input) {
-    if (input <= 100 || input > 1) {
-        fillBoard(input);
-    } else {
-        alert("mt quadrado");
-    }
-}
 
+//Botão de Tamanho
 const sizeButton = document.querySelector('#setSize');
 sizeButton.addEventListener('mouseover', () => {
     sizeButton.setAttribute('style', 'background-color: rgb(180,180,180); cursor: pointer;');
@@ -56,8 +59,64 @@ sizeButton.addEventListener('mouseout', () => {
     sizeButton.setAttribute('style', 'background-color: white;');
 });
 
-const resetButton = document.querySelector('#resetButton');
-resetButton.addEventListener('click', () => {
-    square.setAttribute('style','background-color: white;');
+//Botão Preto
+const blackButton = document.querySelector('#blackButton');
+blackButton.addEventListener('mouseover', () => {
+    blackButton.setAttribute('style', 'background-color: rgb(180,180,180); cursor: pointer;');
 });
+
+blackButton.addEventListener('mouseout', () => {
+    blackButton.setAttribute('style', 'background-color: white;');
+});
+
+//Botão Cinza
+const greyButton = document.querySelector('#greyButton');
+greyButton.addEventListener('mouseover', () => {
+    greyButton.setAttribute('style', 'background-color: rgb(180,180,180); cursor: pointer;');
+});
+
+greyButton.addEventListener('mouseout', () => {
+    greyButton.setAttribute('style', 'background-color: white;');
+});
+
+//Botão Branco
+const whiteButton = document.querySelector('#whiteButton');
+whiteButton.addEventListener('mouseover', () => {
+    whiteButton.setAttribute('style', 'background-color: rgb(180,180,180); cursor: pointer;');
+});
+
+whiteButton.addEventListener('mouseout', () => {
+    whiteButton.setAttribute('style', 'background-color: white;');
+});
+
+//Botão Random
+const randomButton = document.querySelector('#randomButton');
+randomButton.addEventListener('mouseover', () => {
+    randomButton.setAttribute('style', 'background-color: rgb(180,180,180); cursor: pointer;');
+});
+
+randomButton.addEventListener('mouseout', () => {
+    randomButton.setAttribute('style', 'background-color: white;');
+});
+
+//Botão Reset
+const resetButton = document.querySelector('#resetButton');
+resetButton.addEventListener('mouseover', () => {
+    resetButton.setAttribute('style', 'background-color: rgb(180,180,180); cursor: pointer;');
+});
+
+resetButton.addEventListener('mouseout', () => {
+    resetButton.setAttribute('style', 'background-color: white;');
+});
+
+fillBoard(16);
+
+function changeSize(input) {
+    if (input <= 100 || input > 1) {
+        fillBoard(input);
+    } else {
+        alert("mt quadrado");
+        console.log('grande ein');
+    }
+}
 
